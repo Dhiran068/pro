6910d0b931509e20197ed3598d701cb6dc220ecb
Dhiran068 <nagendhirann7845@gmail.com>
2025-02-11 19:17:33 +0530

    Add

---
+++ index.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner m = new Scanner(System.in);
+        int a = m.nextInt();
+        System.out.println(a);
+        System.out.println("Hello World");
+    }
+}
